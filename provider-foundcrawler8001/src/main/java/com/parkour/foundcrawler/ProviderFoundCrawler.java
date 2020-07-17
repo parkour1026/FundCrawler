5849f0e6d644e6a3e9719bbda138d3709b8f21d5
parkour <836462915@qq.com>
2020-07-17 22:31:33 +0800

    构建微服务架构中2

--- provider-foundcrawler8001/src/main/java/com/parkour/foundcrawler/ProviderFoundCrawler.java
+++ provider-foundcrawler8001/src/main/java/com/parkour/foundcrawler/ProviderFoundCrawler.java
@@ -1,19 +1,18 @@
 package com.parkour.foundcrawler;
 
-import com.parkour.foundcrawler.myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author com.parkour
  */
 @SpringBootApplication
 @EnableEurekaClient
-@RibbonClient(name = "PROVIDER-FOUNDCRAWLER", configuration = MyselfRule.class)
-public class CustomerFoundCrawler {
+@EnableDiscoveryClient
+public class ProviderFoundCrawler {
     public static void main(String[] args) {
-        SpringApplication.run(CustomerFoundCrawler.class, args);
+        SpringApplication.run(ProviderFoundCrawler.class, args);
     }
 }
--- provider-foundcrawler8002/src/main/java/com/parkour/foundcrawler/ProviderFoundCrawler2.java
+++ provider-foundcrawler8002/src/main/java/com/parkour/foundcrawler/ProviderFoundCrawler2.java
@@ -1,4 +1,4 @@
-package com.parkour;
+package com.parkour.foundcrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
